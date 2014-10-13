f4ca573777141f8d0cf0677be164d95e1af23c2c
Cesar Hincapié <cesar.hincapie@mail.utoronto.ca>
2014-10-13 03:25:58 -0400

    R programming - Quiz 2

---
+++ r_prog_quiz2.R
@@ -0,0 +1,52 @@
+# Coursera - R programming - Quiz 2
+# Programmer: Cesar
+# Date: October 13, 2014
+
+# Q1
+cube <- function (x, n) {
+  x^3
+}
+cube(3)
+
+# Q2
+x <- 1:10
+if(x > 5) {
+  x <- 0
+}
+x
+
+# Q3
+f <- function(x) {
+      g <- function(y) {
+            y + z
+      }
+      z <- 4
+      x + g(x)
+}
+z <- 10
+f(3)
+
+# Q4
+x <- 5
+y <- if(x < 3) {
+      NA
+} else {
+      10
+}
+y
+
+# Q5
+h <- function(x, y = NULL, d = 3L) {
+      z <- cbind(x, d)
+      if(!is.null(y))
+            z <- z + y
+      else
+            z <- z + f
+      g <- x + y / z
+      if(d == 3L)
+            return(g)
+      g <- g + 10
+      g
+}
+
+h(2)
